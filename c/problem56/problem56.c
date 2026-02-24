7b56bddcfd3d33fece9fe8ddd8b72ef20d9ebe7b
RoyS122 <roysimon.sco@gmail.com>
2026-02-24 22:53:15 +0100

    add: soluce to 56

--- c/problem56/problem56.c
+++ c/problem56/problem56.c
@@ -3,53 +3,89 @@
 #include <math.h> 
 #include <pthread.h>
 
-
-unsigned long int digitSum(unsigned int nbr) {
-    unsigned long int sum = 0;
-    while(nbr >= 10) {
-        sum +=  nbr % 10;
-        nbr /= 10;
-    }
-    return sum + nbr;
-} 
-
-
-// typedef struct
-// {
-// 	unsigned int *array;
-// 	unsigned int length;
-// 	unsigned int max;
-// 	unsigned int min;
-// } DynArray;
+#define DIGITSBACTH 1000
 
 typedef struct {
-    unsigned int input;
-    unsigned long  output;
-} DigitSumTask;
+    unsigned char *arr;
+    unsigned long length;
+} BigInt;
 
 
-void *threadSum(void *arg) {
-    DigitSumTask *t = (DigitSumTask*) arg;
-	t->output = digitSum(t->input);
-    printf("thread:%i, result: %i.\n", t->input, t->output);
-	// Arr�t propre du thread
-	pthread_exit(EXIT_SUCCESS);
+int appendDigits(BigInt *d, unsigned char a ) {
+    if(d->length == 0) {
+        d->arr = (unsigned char *)malloc(DIGITSBACTH);
+    }else{
+        if (d->length % DIGITSBACTH == 0) {
+            d->arr = (unsigned char *)realloc(d->arr,d->length + DIGITSBACTH);
+        } 
+    }
+    d->arr[d->length] = a;
+    d->length += 1;
+    return 0;
+}
+
+BigInt createDigits(unsigned char v) {
+    BigInt res = {NULL, 0};
+    while (v > 10) {
+        appendDigits(&res, v %10);
+        v /= 10;
+    }
+    appendDigits(&res, v);
+    
+    return res;
 }
 
 
+
+int Mult(BigInt *a,unsigned short b) { 
+    int carry = 0;
+    for(int i = 0; i < a->length; i ++) {
+        int product = (a->arr[i] * b) + carry;
+        a->arr[i] = product % 10;
+        carry = product / 10;
+    }
+
+    while(carry > 0) {
+        appendDigits(a, carry%10);
+        carry /= 10;
+    }
+    return 0;
+}
+
+int Power(BigInt *a, unsigned char b, unsigned char origin_a) {
+    if (b > 1) {
+        Mult(a, origin_a);
+        Power(a, b - 1, origin_a);
+    }
+    return 0;
+}
+
+unsigned long long DigitSum(BigInt *a) {
+    unsigned long long res = 0;
+    for(unsigned long i = 0; i < a->length; i ++) {
+        res += a->arr[i];
+    }
+    return res;
+} 
+
+
+
+
 unsigned long  solveProblem() {
+    unsigned long res = 0;
 
-    pthread_t thread1;
-    pthread_t thread2;
-    DigitSumTask t1 = {input: 3432693, 0};
-    DigitSumTask t2 = {1782934, 0};
-    pthread_create(&thread1, NULL, threadSum, &t1);
-    pthread_create(&thread2, NULL, threadSum, &t2);
-
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-    if (t1.output > t2.output) return t1.output;
-    return t2.output ;
+    for(unsigned int i = 1; i < 100; i ++) {
+        for(unsigned int j = 1; j < 100; j ++) {
+            BigInt a = createDigits(i);
+            Power(&a, j, i);
+            unsigned long output = DigitSum(&a);
+            if(res < output) {
+                res = output;
+            }
+        }    
+    }
+   
+    return res;
 }
 
 void wait() {
@@ -57,11 +93,7 @@ void wait() {
     getchar();
 }
 
-int main() {
-    // pthread_t thread1;
-    // pthread_create(&thread1, NULL, thread_1, NULL);
-    // pthread_join(thread1, NULL);
-   
+int main() { 
     printf("%i \n", solveProblem());
     wait();
     
